2782af0b019655b1bf3692947347affd8f375fdf
gitmangal <135738278+gitmangal@users.noreply.github.com>
2024-02-14 23:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,11 +53,9 @@ def load_overall_analysis():
     x_labels = temp_df['x_axis'].iloc[::nth_value]
     ax3.set_xticks(x_values)
     ax3.set_xticklabels(x_labels, rotation=45)  # Rotate x-axis labels for better readability
-    # Show plot in Streamlit
+    # Show plot inStreamlit
     st.pyplot(fig3)
-
     
-    # top sector 
     st.header('Top 10 Sectors')
     df['vertical']=df['vertical'].str.replace('eCommerce','E-commerce')
     df['vertical']=df['vertical'].str.replace('ECommerce','E-commerce')
@@ -66,7 +64,8 @@ def load_overall_analysis():
     sanalysis = df.groupby('vertical')['amount'].sum().sort_values(ascending=False).head(10)
     fig4, ax4 = plt.subplot
     ax4.bar(sanalysis.index,sanalysis.values)
-    st.plot(fig4)
+    st.pyplot(fig4)
+    
 def load_investor_details(investor):
     st.title(investor)
     # load the recent 5 investments of the investor
