a2f26898a409bd4d528afd6d692e059fcf2ee857
gitmangal <135738278+gitmangal@users.noreply.github.com>
2024-02-14 17:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,6 @@ def load_overall_analysis():
     temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
     fig3, ax3 = plt.subplots()
     ax3.plot(temp_df['x_axis'], temp_df['amount'])
-
-    st.pyplot(fig3)
     ax3.set_xlabel('Month-Year')
     ax3.set_ylabel('Amount')
     
