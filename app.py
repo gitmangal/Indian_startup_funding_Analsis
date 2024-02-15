5acacc44b8f68667acda5d267fe17596a7b1227a
gitmangal <135738278+gitmangal@users.noreply.github.com>
2024-02-15 18:00:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,10 +78,7 @@ def load_overall_analysis():
         st.pyplot(fig4)
         
     with col6:
-        tpinvestors = investordf.groupby('index')['amount'].sum().sort_values(ascending=False).head(5)
-        fig5, ax5 = plt.subplots()
-        ax4.bar(tpinvestors.index, tpinvestors.values)
-        st.pyplot(fig5)
+       
 
 def load_investor_details(investor):
     st.title(investor)
