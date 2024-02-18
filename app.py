91dd1ecce3f8e317c6087ddc85245b5acd1db0c8
gitmangal <135738278+gitmangal@users.noreply.github.com>
2024-02-18 09:59:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,11 +164,8 @@ def load_investor_details(investor):
 def load_startup_details(select_startup):
     st.header(select_startup)
     startabout = df[df['startup'].str.contains(select_startup)][['year','vertical','subvertical','city','round']]
-    
     st.dataframe(startabout)
-
     inv_series = df[df['startup'].str.contains(select_startup)][[['date','investors','round','amount']]
-    
     st.dataframe(sum_series)
     
 
